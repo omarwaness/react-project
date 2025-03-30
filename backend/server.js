105fb2c86bea6e04957042c00a07d99e90670a31
omarwaness <o5v4w2000@gmail.com>
2025-03-30 16:55:33 +0100

    committed

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: "http://localhost", credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173", "https://react-project-khaki-eight.vercel.app"], credentials: true }));
 app.use(cookieParser());
 
 // Connect to MongoDB
